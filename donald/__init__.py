a983210113c2ffe30151e20884836d6e828a77af
Kirill Klenov <horneds@gmail.com>
2016-12-28 09:41:40 +0300

    Bump version: 0.1.1 → 0.1.2

--- donald/__init__.py
+++ donald/__init__.py
@@ -16,7 +16,7 @@ logger.addHandler(handle)
 from .core import Donald # noqa
 
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __project__ = "Donald"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
