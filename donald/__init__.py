20ee111d6b5b1ff76bcfdaa460b9083953456a45
Kirill Klenov <horneds@gmail.com>
2017-02-15 13:36:12 +0300

    Bump version: 0.2.2 → 0.2.3

--- donald/__init__.py
+++ donald/__init__.py
@@ -16,7 +16,7 @@ logger.addHandler(handle)
 from .core import Donald, CronTab as crontab # noqa
 
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __project__ = "Donald"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
