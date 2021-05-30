992eda6625407c356e5df4dcc4d0bdd0903d766f
Kirill Klenov <horneds@gmail.com>
2021-05-30 15:26:20 +0300

    Bump version: 0.6.0 → 0.7.0

--- donald/__init__.py
+++ donald/__init__.py
@@ -18,7 +18,7 @@ logger.addHandler(handle)
 from .core import Donald, CronTab as crontab, run_donald # noqa
 
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 __project__ = "Donald"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
