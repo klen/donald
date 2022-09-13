2a91a01b5b601401a939319af380873adbbf653a
Kirill Klenov <horneds@gmail.com>
2022-09-13 14:55:04 +0300

    Bump version: 0.15.0 → 0.15.1

--- donald/__init__.py
+++ donald/__init__.py
@@ -18,7 +18,7 @@ from .core import Donald, run_donald
 
 __all__ = "crontab", "Donald", "run_donald"
 
-__version__ = "0.15.0"
+__version__ = "0.15.1"
 __project__ = "Donald"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
