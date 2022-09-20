659786c71dd85a9e7c75beeef16d8f5d7129a007
Kirill Klenov <horneds@gmail.com>
2022-09-20 20:21:15 +0300

    fix: wip

--- donald/__init__.py
+++ donald/__init__.py
@@ -8,14 +8,6 @@ __license__ = "BSD"
 import logging
 
 logger = logging.getLogger(__name__)
-handle = logging.StreamHandler()
-handle.setFormatter(
-    logging.Formatter(
-        "[%(asctime)-15s] [%(process)d] [%(levelname)s] %(message)s",
-        "%Y-%m-%d %H:%M:%S",
-    )
-)
-logger.addHandler(handle)
 
 from threading import local
 
