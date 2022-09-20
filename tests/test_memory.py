d637771bf9c187ca379cc05a647023123671c2a3
Kirill Klenov <horneds@gmail.com>
2022-09-20 17:35:33 +0300

    fix: tests

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -51,7 +51,7 @@ async def test_timeout(sleep, checklog):
 
 async def test_nested_tasks(sleep, checklog):
     nested_task.submit()
-    await sleep()
+    await sleep(2e-2)
     assert checklog("Run async_task nested", min_count=3)
 
 
