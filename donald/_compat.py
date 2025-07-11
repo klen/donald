d0e51597905a9adfee946bf369382b7b7bc6fa33
Kirill Klenov <horneds@gmail.com>
2025-07-11 11:43:24 +0300

    feat: add compatibility module for asyncio timeout 🚀
    This commit introduces a new `_compat.py` file in the `donald` directory. The module provides compatibility for the `asyncio.timeout` function across different Python versions (3.9 and 3.10). It uses a try-except block to import `async_timeout` from the appropriate module, ensuring that the code remains compatible with both versions. The file also includes a noqa directive to suppress specific linting warnings about unused imports.

---
+++ donald/_compat.py
@@ -0,0 +1,7 @@
+try:
+    from asyncio import timeout as async_timeout  # type: ignore[attr-defined]
+except ImportError:  # python 39, 310
+    from async_timeout import timeout as async_timeout  # type: ignore[assignment, no-redef]
+
+
+# ruff: noqa: F401
